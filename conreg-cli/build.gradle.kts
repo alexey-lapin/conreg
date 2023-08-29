aa2880d7e5602b0753f3d93fc2c12fed6831eddb
Alexey Lapin <alexey-lapin@protonmail.com>
2023-08-29 20:27:13 +0200

    fix picocli deps

--- conreg-cli/build.gradle.kts
+++ conreg-cli/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 dependencies {
-    annotationProcessor(libs.picocli)
+    annotationProcessor(libs.picocli.codegen)
 
     implementation(project(":conreg-core"))
     implementation(project(":conreg-httpclient-jdk"))
 
-    implementation(libs.picocli.codegen)
+    implementation(libs.picocli)
     implementation(libs.jackson.databind)
     implementation(libs.jackson.datatype.jsr310)
 
